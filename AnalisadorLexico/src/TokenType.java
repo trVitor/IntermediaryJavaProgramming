d217db04d2750aab801a216b21d02728af6dc83c
Vitor TR <102200168+trVitor@users.noreply.github.com>
2024-05-22 19:27:40 -0300

    Atualização da classe TokenType.java
    Necessário o incremento de mais uma palavra na enum, para reconhecimento de strings

--- AnalisadorLexico/src/TokenType.java
+++ AnalisadorLexico/src/TokenType.java
@@ -4,5 +4,5 @@ public enum TokenType {
      * Enumeração TokenType:
      * Define os diferentes tipos de tokens possíveis, como números, identificadores, palavras-chave, operadores, etc.
      */
-    NUMBER, ID, KEYWORD, OP, LPAREN, RPAREN, LBRACE, RBRACE, SEMICOLON, COLON, COMMA, DOT, WHITESPACE, NEWLINE, COMMENT, MISMATCH
+    NUMBER, ID, KEYWORD, OP, LPAREN, RPAREN, LBRACE, RBRACE, SEMICOLON, COLON, COMMA, DOT, WHITESPACE, NEWLINE, COMMENT,STRING, MISMATCH
 }
